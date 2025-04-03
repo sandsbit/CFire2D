52cc719bbdd30e0cf7d3bcfbe45451d507e77b6d
Nikita Serba <nikitaserba@icloud.com>
2025-04-03 15:09:09 +0300

    Add exception for unsupported audio format

---
+++ CFire2D/audio/exceptions/unsupported_audio_format.cpp
@@ -0,0 +1,24 @@
+/**
+* Copyright (C) 2025 Nikita Serba. All rights reserved.
+ *
+ * This file is part of CFire 2D.
+ *
+ * CFire 2D is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * CFire 2D is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License and GNU Lesser General
+ * Public License along with CFire 2D. If not, see https://www.gnu.org/licenses/.
+ */
+
+#include "unsupported_audio_format.h"
+
+namespace c2d::exceptions {
+ const char * unsupported_audio_format::what() const noexcept {
+  return "audio file format isn't supported";
+ }
+}
